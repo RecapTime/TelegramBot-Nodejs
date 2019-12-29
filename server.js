dd73e093df0d2058fd8c333f7ec3231080a93577
Glitch (hello-express) <none>
2019-12-29 10:09:25 +0000

    🎠👼 Checkpoint ./server.js:4790460/3553

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require("express");
 const app = express();
 const TelegramBot = require("node-telegram-bot-api");
 const bodyParser = require("body-parser");
+const { QueueManager } = require("node-telegram-operation-manager");
+
 
 // Pull token from environment variables, start with your Telegram Bot token
 const token = process.env.TGBOT_TOKEN;
 
-//
+// Get url and port from env
 const url = process.env.APP_URL;
 const port = process.env.PORT;
 
@@ -21,7 +23,7 @@ bot.setWebHook(`${url}/webhook/tgbot-${token}`);
 app.use(bodyParser.json());
 
 // We are receiving updates at the route below!
-app.post(`/bot${token}`, (req, res) => {
+app.post(`/webhook/tgbot-${token}`, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
@@ -30,11 +32,25 @@ app.post(`/bot${token}`, (req, res) => {
 app.listen(port, () => {
   // Test
   console.log(
-    "Express.js endpoint now listening on " + process.env.APP_URL + ":" + process.env.PORT
+    "Express.js endpoint now listening on " +
+      process.env.APP_URL +
+      ":" +
+      process.env.PORT
   );
 });
 
-// Just to ping!
-bot.on("message", msg => {
-  bot.sendMessage(msg.chat.id, "I am alive!");
+// Matches "/echo [whatever]"
+bot.onText(/\/start/, msg => {
+  // 'msg' is the received Message from Telegram
+
+  const chatId = msg.chat.id;
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(
+    chatId,
+    "Welcome to the *Recap Time bot!*" +
+      "\n\n" +
+      "Currently, the bot is still work in process and everyone can",
+    { parse_mode: "markdown" }
+  );
 });
