5d1d770c3c6eff88ff26ae934955dad20d757d47
Glitch (hello-express) <none>
2019-12-29 09:46:25 +0000

    ☂️🛫 Checkpoint ./package.json:4790460/50 ./server.js:4790460/521 ./.env.test:4790460/3342 ./.gitignore:4790460/1671

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const TelegramBot = require("node-telegram-bot-api");
 
 // Pull token from environment variables
 const token = process.env.TGBOT_TOKEN;
+const url = process.env.APP_URL;
+const port = process.env.PORT;
+
 
 // Start the bot
 const bot = new TelegramBot(token, { polling: true });
@@ -14,15 +17,7 @@ bot.onText(/\/start/, msg => {
     msg.chat.id,
     "Welcome to the **Recap Time Bot**! The bot is currently work in progress. More features soon!" +
       "\n\n" +
-      "`Version 0.5.0-canary Released on 12-28-2019 by Andrei Jiroh Halili (@AJHalili2006)`",
+      "`Version Info: Version 0.5.0-canary Released on TBA by Andrei Jiroh Halili (@AJHalili2006)`",
     { parse_mode: "Markdown" }
-  );
-});
-
-bot.on("message", msg => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(
-    chatId,
-    "I heard you about that. Processing data..."
   );
 });
