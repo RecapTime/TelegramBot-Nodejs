d6ed61d49bff83ebd62db7363a3c14330abfa768
Glitch (hello-express) <none>
2019-12-29 09:57:26 +0000

    🤘🏝 Checkpoint ./server.js:4790460/3379 ./package.json:4790460/2831 ./LICENSE.md:4790460/1094 ./README.md:4790460/201 ./.env.test:4790460/441

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Recap Time Bot
 ===========
 
+## Setup
+
+### Deploy on Glitch.com
+1. [Remix the project](https://glitch.com/edit/#!/remix/handsome-sheet), copy contents from `.env.test` file.
+2. Edit the `server.js` as usual.
+3. It auto-deploys
--- server.js
+++ server.js
@@ -2,22 +2,39 @@
 const express = require("express");
 const app = express();
 const TelegramBot = require("node-telegram-bot-api");
+const bodyParser = require("body-parser");
 
-// Pull token from environment variables
+// Pull token from environment variables, start with your Telegram Bot token
 const token = process.env.TGBOT_TOKEN;
+
+//
 const url = process.env.APP_URL;
 const port = process.env.PORT;
 
-
 // Start the bot
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token);
 
-bot.onText(/\/start/, msg => {
-  bot.sendMessage(
-    msg.chat.id,
-    "Welcome to the **Recap Time Bot**! The bot is currently work in progress. More features soon!" +
-      "\n\n" +
-      "`Version Info: Version 0.5.0-canary Released on TBA by Andrei Jiroh Halili (@AJHalili2006)`",
-    { parse_mode: "Markdown" }
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`${url}/webhook/tgbot-${token}`);
+
+// parse the updates to JSON
+app.use(bodyParser.json());
+
+// We are receiving updates at the route below!
+app.post(`/bot${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Start Express Server
+app.listen(port, () => {
+  // Test
+  console.log(
+    "Express.js endpoint now listening on " + process.env.APP_URL + ":" + process.env.PORT
   );
+});
+
+// Just to ping!
+bot.on("message", msg => {
+  bot.sendMessage(msg.chat.id, "I am alive!");
 });
