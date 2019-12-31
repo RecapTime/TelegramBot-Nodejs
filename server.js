8bb8677e15f15bbe74602dfb2519e74f4a0bea1e
Glitch (handsome-sheet) <none>
2019-12-31 06:32:18 +0000

    🎏🌵 Checkpoint ./server.js:4790460/1644 ./views/manifest.json:4790460/1112 ./views/index.html:4790460/9173 ./public/app.js:4790460/4028

--- server.js
+++ server.js
@@ -29,70 +29,7 @@ app.post(`/webhook/tgbot-${token}`, (req, res) => {
   res.sendStatus(200);
 });
 
-app.use("/static", express.static("public"));
-app.use(express.static("views"));
-
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/offline", function (request, response) {
-  response.sendFile(__dirname + '/views/offline.html');
-});
-
-app.get("/manifest.json", function (request, response) {
-  response.sendFile(__dirname + '/views/manifest.json');
-});
-
-app.get("*", function (request, response) {
-  response.sendFile(__dirname + '/views/404.html');
-});
-
-function clientErrorHandler(err, req, res, next) {
-  if (req.xhr) {
-    res
-      .status(500)
-      .send({
-        error:
-          "Something went berserk on our side. Contact Bot Support or hang tight an few hours."
-      });
-  } else {
-    next(err);
-  }
-}
-
-// Start Express Server
-app.listen(port, () => {
-  // Test
-  console.log(
-    "Express.js endpoint now listening on " +
-      process.env.APP_URL +
-      ":" +
-      process.env.PORT
-  );
-});
-
-// Matches "/echo [whatever]"
-bot.onText(/\/start/, msg => {
-  // 'msg' is the received Message from Telegram
-
-  const chatId = msg.chat.id;
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(
-    chatId,
-    "Welcome to the *Recap Time bot!*" +
-      "\n\n" +
-      "Currently, the bot is still work in process and everyone can contribute to improve the bot." +
-      "\n\n" +
-      "To contribute, use /contribute for details.",
-    { parse_mode: "markdown" }
-  );
-});
-
-bot.onText(/\/contribute/, msg =>
-  bot.sendMessage(msg.chat.id, "We love contributions in order to make this bot more powerful", { parse_mode: "markdown" })
-);
+// Documentation Files
 
 // Remove the code below for local deployments and deployments outside Glitch.com
 process.on("SIGTERM", function() {
