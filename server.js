e52d57eae4a209d8f4d9193b392cb9ee1e94e185
Glitch (handsome-sheet) <none>
2020-01-04 09:36:13 +0000

    👹🤑 Checkpoint ./server.js:4790460/1963 ./index.html:4790460/9527

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 // init project
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs')
-
+const fs = require("fs");
 
 // Glitch Slug
-const GLITCH_PROJECT_SLUG = process.PROJECT_DOMAIN || "handsome-sheet"
+const GLITCH_PROJECT_SLUG = process.PROJECT_DOMAIN || "handsome-sheet";
 
-app.get('/', (res, rep) =>
-       res.sendFile(__dirname + "/docs/index.html")
-);
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
+app.get("/docs", (req, res) => res.sendFile(__dirname + "/docs/index.html"));
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your Express app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+  console.log(
+    "Your Express app is listening on port " + listener.address().port
+  );
 });
 
 // Remove the code below for local deployments and deployments outside Glitch.com
