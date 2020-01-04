2197fc693d84715f26529badd1dc29d4c05ed97d
Glitch (handsome-sheet) <none>
2020-01-04 09:00:53 +0000

    👻🏎 Checkpoint ./server.js:4790460/509 ./views/index.html:4790460/553

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+// init project
+const express = require('express');
+const app = express();
+const fs = require('fs')
+
 
 // Glitch Slug
 const GLITCH_PROJECT_SLUG = process.PROJECT_DOMAIN || "handsome-sheet"
+
+//
+app.get("/docs", function (request, response) {
+  response.header("Cache-Control", "max-age=0");
+  const files = {};
+  getFilesInDirectory('md').sort().forEach(path => {
+    const fileName = path.replace(/md\/(.*)\.md/, '$1'); //trim off "md/" and ".md"
+    files[fileName] = fs.readFileSync(path, 'utf8');
+  });
+  response.send(files);
+});
+
 
 // Remove the code below for local deployments and deployments outside Glitch.com
 // It's better to get your own copy of this project.
