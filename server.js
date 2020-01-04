cb50eba08e9913a321b25e81ee3cf44f072fde24
Glitch (handsome-sheet) <none>
2020-01-04 09:12:53 +0000

    👐🏃 Checkpoint ./views/index.html:4790460/3828 ./server.js:4790460/575

--- server.js
+++ server.js
@@ -7,17 +7,37 @@ const fs = require('fs')
 // Glitch Slug
 const GLITCH_PROJECT_SLUG = process.PROJECT_DOMAIN || "handsome-sheet"
 
+function getFilesInDirectory(dir) {
+  return [].concat(...fs.readdirSync(dir).map(name => {
+    const path = dir + '/' + name;
+    const stats = fs.statSync(path);
+    if (stats.isDirectory()) {
+      return getFilesInDirectory(path);
+    } else if (stats.isFile()) {
+      return [path];
+    } 
+    return [];
+  }));
+}
+
 //
 app.get("/docs", function (request, response) {
   response.header("Cache-Control", "max-age=0");
   const files = {};
   getFilesInDirectory('md').sort().forEach(path => {
-    const fileName = path.replace(/md\/(.*)\.md/, '$1'); //trim off "md/" and ".md"
+    const fileName = path.replace(/docs\/(.*)\.md/, '$1'); //trim off "md/" and ".md"
     files[fileName] = fs.readFileSync(path, 'utf8');
   });
   response.send(files);
 });
 
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your Express app is listening on port ' + listener.address().port);
+});
 
 // Remove the code below for local deployments and deployments outside Glitch.com
 // It's better to get your own copy of this project.
