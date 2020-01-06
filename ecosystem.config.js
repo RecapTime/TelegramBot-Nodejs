dbbdad6b74bd5af784116547425161cdf228a5cf
Glitch (handsome-sheet) <none>
2020-01-06 10:04:42 +0000

    📚🎿 Checkpoint ./app.json:4790460/666 ./ecosystem.config.js:4790460/32

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,32 +1,29 @@
 module.exports = {
   apps : [{
-    name: 'TELEGRAM_BOT_SERVER',
-    script: 'server.js',
+    name: 'Telegram Bot Server',
+    script: 'app.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     args: 'one two',
     instances: 1,
     autorestart: true,
-    watch: true,
-    max_memory_restart: '1G',
+    watch: false,
+    max_memory_restart: '512M',
     env: {
       NODE_ENV: 'development'
     },
     env_production: {
-      NODE_ENV: 'production',
-      TGBOT_TOKEN: 'yourtokenhere'
+      NODE_ENV: 'production'
     }
   }],
 
   deploy : {
     production : {
       user : 'node',
-      // Change the host Ip address
       host : '212.83.163.1',
       ref  : 'origin/master',
-      repo : 'git@gitlab.com:MadeByThePinsTeam-DevLabs/RecapTime-Staff/recaptime-tgbot-nodejs.git',
+      repo : 'git@github.com:repo.git',
       path : '/var/www/production',
-      "pre-deploy": 'git push',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
